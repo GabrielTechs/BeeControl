8abfca6d26ce698c7892c17a3c326a5f367b1516
Raycris <raycris97@gmail.com>
2019-03-04 17:37:56 -0400

    pantallas registro agregado y login arreglado

--- BeeControl/app/src/main/java/com/example/caleb/beecontrol/RegistroActivity.kt
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/RegistroActivity.kt
@@ -3,10 +3,10 @@ package com.example.caleb.beecontrol
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegistroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_registro)
     }
 }
