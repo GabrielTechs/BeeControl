7ad4991768daadbe103a0fb00e1de22b3356d18f
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2019-03-13 17:33:12 -0400

    pantallas de asistencia y viajes agregadas

---
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/AssistanceActivity.kt
@@ -0,0 +1,42 @@
+package com.example.caleb.beecontrol
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
+import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_assistance.*
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
+
+
+class AssistanceActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assistance)
+    }
+    fun login(view:View){
+        startActivity(Intent(this, MenuActivity::class.java))
+    }
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun clickDataPicker(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            // Display Selected date in Toast
+            Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
+        }, year, month, day)
+        dpd.show()
+    }
+}
