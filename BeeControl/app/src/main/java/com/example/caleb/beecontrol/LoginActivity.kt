18f9314b74b75d965c180a3dd4a0bad49e4972d5
Unknown <belaczemog@gmail.com>
2019-03-21 22:42:53 -0400

    cambios firebase

--- BeeControl/app/src/main/java/com/example/caleb/beecontrol/LoginActivity.kt
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/LoginActivity.kt
@@ -4,18 +4,55 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity()
 {
 
+    var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?)
     {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+       // FirebaseApp.initializeApp(this);
+
+
+        txtRegister.setOnClickListener(){
+            var intent = Intent(this,RegistroActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
-    fun register(view:View){
-        startActivity(Intent(this, RegistroActivity::class.java))
+
+
+        fun login(view: View){
+                var email = txtEmailR.text.toString()
+                var password = txtPasswordR.toString()
+
+                signIn(email, password)
+        }
+
+
+
+
+
+    private fun signIn(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {task->
+            if(task.isSuccessful){
+
+                var intent = Intent(this, PProfileActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+
     }
+
+
 
 }
