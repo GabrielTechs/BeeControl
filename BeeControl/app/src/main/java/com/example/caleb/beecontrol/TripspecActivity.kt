d5aa2e0a3f18fffe0deb4cc69b95c6e6bcdaf7c7
Gabriel Henriquez Alguacil <gabha2107@gmail.com>
2019-03-27 17:29:04 -0400

    Activity tripspec & recyclerview for activity trip

---
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/TripspecActivity.kt
@@ -0,0 +1,12 @@
+package com.example.caleb.beecontrol
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TripspecActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tripspec)
+    }
+}
