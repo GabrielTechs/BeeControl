8917de2dfed7a60ce4805f93aa73d0f28d8633c1
Juan Mariñez <jmarinez@sergioguzmansoft.com>
2019-04-01 21:18:20 -0500

    layout agregado a activity trip

--- BeeControl/app/src/main/java/com/example/caleb/beecontrol/TripActivity.kt
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/TripActivity.kt
@@ -20,7 +20,7 @@ class TripActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_trip)
         setUpRecyclerView()
     }
 
