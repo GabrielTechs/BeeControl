a1abd6ea69cad5afc09832d23e3d5460d1a6f833
Juan Mariñez <jmarinez@sergioguzmansoft.com>
2019-04-10 04:43:18 -0400

    implementacion beacon

--- BeeControl/app/src/main/java/com/example/caleb/beecontrol/MenuActivity.kt
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/MenuActivity.kt
@@ -2,17 +2,32 @@ package com.example.caleb.beecontrol
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.RemoteException
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.app_bar_menu.*
+import org.altbeacon.beacon.*
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.BeaconManager
 
-class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, BeaconConsumer,
+        MonitorNotifier {
+
+    protected val TAG = "MenuActivity"
+    private val PERMISSION_REQUEST_COARSE_LOCATION = 1
+    private val REQUEST_ENABLE_BLUETOOTH = 1
+    private val DEFAULT_SCAN_PERIOD_MS = 6000L
+    private val ALL_BEACONS_REGION = "AllBeaconsRegion"
+    lateinit private var mBeaconManager: BeaconManager
+    lateinit private var mRegion: Region
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +45,45 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onResume(){
+        super.onResume()
+        mBeaconManager = BeaconManager.getInstanceForApplication(this)
+        // Detect the main Eddystone-UID frame:
+        mBeaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT))
+        mBeaconManager.bind(this)
+    }
+
+    override fun onBeaconServiceConnect() {
+        val myBeaconNamespaceId = Identifier.parse("1ef930cc1f0e886be663")
+        val myBeaconInstanceId = Identifier.parse("000000000000")
+        val region = Region("my-beacon-region", myBeaconNamespaceId, myBeaconInstanceId, null)
+        mBeaconManager.addMonitorNotifier(this)
+        try {
+            mBeaconManager.startMonitoringBeaconsInRegion(region)
+        }
+        catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun didDetermineStateForRegion(p0: Int, p1: Region?) {
+
+    }
+
+    override fun didEnterRegion(p0: Region?) {
+        Toast.makeText(this, "I detected a beacon in the region with namespace id " + p0!!.id1 +
+                " and instance id: " + p0.id2, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun didExitRegion(p0: Region?) {
+
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        mBeaconManager.unbind(this)
     }
 
     override fun onBackPressed() {
