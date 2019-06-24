32b9dbedb664b9a66f421e4cb6c78d30bc9b4092
Raycris <raycris97@gmail.com>
2019-06-24 09:22:39 -0400

    Foto de perfil
        <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/> para acceder a la galeria en el (manifest), añadi un boton en el editprofile y en el editProfileActivity.kt  esta el codigo de las imagenes. En cualquier caso les envio el link del video

--- BeeControl/app/src/main/java/com/example/caleb/beecontrol/EditProfileActivity.kt
+++ BeeControl/app/src/main/java/com/example/caleb/beecontrol/EditProfileActivity.kt
@@ -1,17 +1,57 @@
 package com.example.caleb.beecontrol
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.list_employees.*
 
-class EditProfileActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+class EditProfileActivity : AppCompatActivity()
+{
+    var imagen = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
+
     }
-    fun back(view: View){
+    fun back(view: View)
+    {
         startActivity(Intent(this, MenuActivity::class.java))
+        imagen = findViewById(R.id.txtFperfil)
     }
+
+    fun onClick(view: View): Unit
+    {
+        cargarImagen();
+    }
+
+    /*Para importar imagene de cualquier app*/
+    fun cargarImagen() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/"
+        startActivityForResult(intent.createChooser(intent, "Seleccione la aplicacion"), 10)
+
+    }
+    /*Esta parte es para seleccionar las imagenes desde la galería,*/
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK)
+        {
+
+            Uri path = data.getData();
+            imagen.setImageURI(path);
+        }
+    }
+}
+
+private fun Intent.createChooser(intent: Intent, s: String): Intent? {
+
 }
